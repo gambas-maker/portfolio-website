807e1798a14d6736a81d59eb3168f5702445121a
gambas-maker <clement.guibaud@protonmail.com>
2024-09-14 01:58:00 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request
+from flask_mail import Mail, Message
+
+app = Flask(__name__)
+
+@app.route('/', methods=["GET", "POST"])
+def landing_page():
+    if request.method == "POST":
+        pass
+    return render_template("landing_page.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
