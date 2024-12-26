5b4d9892976241af55525967c3c481e0f6fee8b6
Guibaud Clement <guibaud.clement@gmail.com>
2024-12-26 22:28:40 +0100

    contact form + whatsapp setup

---
+++ mail.py
@@ -0,0 +1,18 @@
+# # external library import
+# from flask_mail import Mail, Message
+# import os
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
+# class Config:
+#     MAIL_SMTP = os.getenv('MAIL_SMTP')
+#     MAIL_PORT = int(os.getenv('MAIL_PORT'))
+#     MAIL_USERNAME=os.getenv('MAIL_USERNAME')
+#     MAIL_PASSWORD=os.getenv('MAIL_PASSWORD')
+#     MAIL_USE_TLS = True
+#     MAIL_USE_SSL = False
+#     MAIL_DEBUG = True
+
+# # mail instance creation
+# mail = Mail()
--- main.py
+++ main.py
@@ -1,13 +1,36 @@
 from flask import Flask, render_template, request
-# from flask_mail import Mail, Message
+# from mail import mail, Message, Config
+from flask_mail import Mail, Message
+import os
 
 app = Flask(__name__)
+
+app.config['MAIL_SERVER']="smtp.gmail.com"
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+
+mail = Mail(app=app)
 
 @app.route('/', methods=["GET", "POST"])
 def landing_page():
     if request.method == "POST":
-        pass
+        msg = Message(
+            sender= request.form.get("email"),
+            subject= request.form.get("title"),
+            body= request.form.get("message"),
+            reply_to=request.form.get('email'),
+            recipients=['guibaud.clement@gmail.com']
+        )
+        mail.send(msg)
+        print('Mail sent succesfully!')
     return render_template("landing_page.html")
+
+@app.route('/webhook', methods=["GET", "POST"])
+def webhook():
+    return render_template("")
 
 if __name__ == "__main__":
     app.run(debug=True)
